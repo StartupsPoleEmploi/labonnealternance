d73a5db8129e0fb9c6747a1faf267980775cf93b
guilletmarion <57708683+guilletmarion@users.noreply.github.com>
2020-08-12 16:43:45 +0200

    Update rgpd_modal.js
    Carte https://trello.com/c/qYvkGdZW/512-maj-pop-in-cookies

--- frontend/src/components/shared/rgpd_bar/rgpd_modal.js
+++ frontend/src/components/shared/rgpd_bar/rgpd_modal.js
@@ -32,19 +32,23 @@ export default class RGPDModal extends Component {
 
                     <div className="modal-body">
 
-                        <p>Pour utiliser le service La Bonne Alternance, vous devez accepter la Politique de confidentialité de La Bonne Alternance.</p>
+                        <p>Pour utiliser au mieux le service La Bonne Alternance, vous devez accepter la Politique de confidentialité de La Bonne Alternance.</p>
 
                         <p>
                             La Bonne Alternance dépose des cookies nécessaires au bon fonctionnement du site :<br />
                                 <ul>
-                                    <li>des cookies de mesure d'audience via les sites Google Analytics et Hotjar</li>
-                                    <li>des cookies techniques pour permettre l'enregistrement de vos préférences ou de l'état de votre interface</li>
+                                    <li>Des cookies anonymes de mesure d'usage via le site Google Analytics. Comme des informations sur votre support informatique (catégorie d’appareil, système d’exploitation, navigateur), ou votre navigation (méthode d’accès à notre site, durée de votre visite).<br />
+                                        Ces cookies nous permettent de mesurer les usages du site La bonne alternance, afin de l’améliorer.</li>
+                                    <li>Des cookies anonymes liés à la réalisation d’enquêtes et analyses via l’outil Hotjar.<br />
+                                        Ces cookies nous permettent d’identifier les usagers ayant déjà répondu à une enquête, afin de ne plus les solliciter sur le sujet.</li>
+                                    <li>Des cookies spécifiques à l’outil La bonne alternance.<br />
+                                        Ces cookies permettent l'enregistrement de vos préférences ou de l'état de votre interface</li>
                                 </ul>
                         </p>
 
                         <p>Toutes ces informations ont pour but d'assurer le bon fonctionnement du site et nous permettent également d'améliorer l'intérêt et l'ergonomie de nos services.</p>
 
-                        <p>Pôle emploi et La Bonne Alternance s'engagent à ne pas communiquer vos données personnelles à des organismes tiers.</p>
+                        <p>Notre service s’engage à ne pas communiquer vos données personnelles à des organismes tiers.</p>
 
                         <p className="text-center ">
                             <button onClick={this.acceptRGPD} className="button small-white">Accepter</button>
