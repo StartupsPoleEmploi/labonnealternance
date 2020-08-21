ef3d868a181564b6664aee13791ebe502048525d
Alex Hoyau <a.hoyau@silexlabs.org>
2020-08-21 10:44:32 +0200

    handle navigation messages from the widgets

--- frontend/src/components/widget/widget.js
+++ frontend/src/components/widget/widget.js
@@ -1,14 +1,34 @@
 import React, {useEffect, createRef} from 'react';
+import {useHistory} from 'react-router-dom';
 
 import './widget.scss';
 
 const iframe = createRef();
 
 export default ({url}) => {
+    const history = useHistory();
     useEffect(() => {
+        // create a listener for post message
+        const onPostMessage = (e) => {
+            switch(e.data.type) {
+                case 'goToPage':
+                    history.push(e.data.page);
+                    break;
+                default:
+                    console.error('unknown post message type');
+            }
+        };
+
+        // listen for widget messages
+        window.addEventListener('message', onPostMessage);
+
         // add a css class to the parent in order to prevent scrolls
         iframe.current.parentElement.classList.add('prevent-scrolls');
-        return () => iframe.current.parentElement.classList.remove('prevent-scrolls');
+        return () => {
+            // cleanup
+            iframe.current.parentElement.classList.remove('prevent-scrolls');
+            window.removeEventListener('message', onPostMessage);
+        };
     });
     return <iframe
         ref={iframe}
